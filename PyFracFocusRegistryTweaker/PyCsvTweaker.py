7093d6d5b38a4de05b5c06612b428ecd7640c05b
KhepryQuixote <khepry.quixote@gmail.com>
2016-01-19 14:22:07 -0600

    Adjust logic to load previous pKey/pInt pairs into their respective dictionaries prior to those dictionaries being used during the conversion process. Doing this allows pKey/pInt pairings to be preserved during subsequent conversions.  The pKey/pInt pairing of today will be the same pKey/pInt pairing for each subsequent time period in perpetuity.

--- PyFracFocusRegistryTweaker/PyCsvTweaker.py
+++ PyFracFocusRegistryTweaker/PyCsvTweaker.py
@@ -19,27 +19,50 @@ ffrCsvPurposeFilePath = os.path.join(ffrCsvPathName, ffrCsvFileNamePurpose)
 ffrCsvIngredientsFilePath = os.path.join(ffrCsvPathName, ffrCsvFileNameIngredients)
 
 # process the RegistryUpload.csv file
-
+prvFileName = os.path.join(ffrCsvPathName, '%s_prvKeyed.csv' % os.path.splitext(os.path.basename(ffrCsvMainFilePath))[0])
 tgtFileName = os.path.join(ffrCsvPathName, '%s_intKeyed.csv' % os.path.splitext(os.path.basename(ffrCsvMainFilePath))[0])
+srcFieldnames = ['pKey','JobStartDate','JobEndDate','APINumber','StateNumber','CountyNumber','OperatorName','WellName','Latitude','Longitude','Projection','TVD','TotalBaseWaterVolume','TotalBaseNonWaterVolume','StateName','CountyName','FFVersion','FederalWell']
+tgtFieldnames = srcFieldnames.copy()
 
+maxRows = 0
+flushRows = 10000
+
+if os.path.exists(prvFileName):
+    print ('Importing previous keys from RegistryUpload CSV file: "%s"', prvFileName)
+    print ('Please wait...')
+    print ('')
+    rows = 0
+    with io.open(prvFileName, 'r', encoding='windows-1252', newline='') as csvSource:
+        dictReader = csv.DictReader(csvSource, fieldnames=['pKey','pInt'], delimiter='\t', quoting=csv.QUOTE_MINIMAL)
+        for row in dictReader:
+            rows += 1
+            if rows > 1:
+                pKey = row['pKey'].strip()
+                ffrUploadDict[pKey] = row['pInt']
+            if flushRows > 0 and rows % flushRows == 0:
+                print ('Rows processed: %d' % rows)
+    print ('%s: Rows processed: %d' % (tgtFileName, rows))
+    print ('')
+
+print ('Converting RegistryUpload rows from CSV file: "%s"', prvFileName)
+print ('Please wait...')
+print ('')
 with io.open(tgtFileName, 'w', encoding='windows-1252') as csvTarget:
-    fieldnames = ['pKey','JobStartDate','JobEndDate','APINumber','StateNumber','CountyNumber','OperatorName','WellName','Latitude','Longitude','Projection','TVD','TotalBaseWaterVolume','TotalBaseNonWaterVolume','StateName','CountyName','FFVersion','FederalWell']
-    dictWriter = csv.DictWriter(csvTarget, fieldnames=fieldnames, delimiter='\t', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+    dictWriter = csv.DictWriter(csvTarget, fieldnames=tgtFieldnames, delimiter='\t', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
     dictWriter.writeheader()
     with io.open(ffrCsvMainFilePath, 'r', encoding='windows-1252', newline='') as csvSource:
-        dictReader = csv.DictReader(csvSource, fieldnames=fieldnames, delimiter='\t', quoting=csv.QUOTE_MINIMAL)
+        dictReader = csv.DictReader(csvSource, fieldnames=srcFieldnames, delimiter='\t', quoting=csv.QUOTE_MINIMAL)
         rows = 0
-        maxRows = 0
-        flushRows = 10000
         thisKey = 0
         for row in dictReader:
             if rows > 0:
+                pKey = row['pKey'].strip()
                 try:
-                    pKey = ffrUploadDict[row['pKey'].strip()]
+                    pKey = ffrUploadDict[pKey]
                 except KeyError:
                     thisKey += 1
-                    ffrUploadDict[row['pKey'].strip()] = thisKey
-                    pKey = ffrUploadDict[row['pKey'].strip()]
+                    ffrUploadDict[pKey] = thisKey
+                    pKey = thisKey
                 row['pKey'] = pKey
                 row['JobStartDate'] = row['JobStartDate'].strip()[:10]
                 row['JobEndDate'] = row['JobEndDate'].strip()[:10]
@@ -52,10 +75,48 @@ with io.open(tgtFileName, 'w', encoding='windows-1252') as csvTarget:
     print ('%s: Rows processed: %d' % (tgtFileName, rows))
     print ('')
 
+# output dictionary for later use the next time this program is executed
+print ('Outputting previous keys to CSV file: "%s"', prvFileName)
+print ('Please wait...')
+print ('')
+rows = 0
+with io.open(prvFileName, 'w', encoding='windows-1252') as csvTarget:
+    dictWriter = csv.DictWriter(csvTarget, fieldnames=['pKey','pInt'], delimiter='\t', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+    dictWriter.writeheader()
+    rows += 1
+    for key, value in ffrUploadDict.items():
+        rows += 1
+        dictWriter.writerow({'pKey': key, 'pInt': value})
+        if flushRows > 0 and rows % flushRows == 0:
+            print ('Rows processed: %d' % rows)
+    print ('%s: Rows processed: %d' % (prvFileName, rows))
+    print ('')
+
 # process the RegistryUploadPurpose.csv file
 
+prvFileName = os.path.join(ffrCsvPathName, '%s_prvKeyed.csv' % os.path.splitext(os.path.basename(ffrCsvPurposeFilePath))[0])
 tgtFileName = os.path.join(ffrCsvPathName, '%s_intKeyed.csv' % os.path.splitext(os.path.basename(ffrCsvPurposeFilePath))[0])
 
+if os.path.exists(prvFileName):
+    print ('Importing previous keys from RegistryPurpose CSV file: "%s"', prvFileName)
+    print ('Please wait...')
+    print ('')
+    rows = 0
+    with io.open(prvFileName, 'r', encoding='windows-1252', newline='') as csvSource:
+        dictReader = csv.DictReader(csvSource, fieldnames=['pKey','pInt'], delimiter='\t', quoting=csv.QUOTE_MINIMAL)
+        for row in dictReader:
+            rows += 1
+            if rows > 1:
+                pKey = row['pKey'].strip()
+                ffrPurposeDict[pKey] = row['pInt']
+            if flushRows > 0 and rows % flushRows == 0:
+                print ('Rows processed: %d' % rows)
+    print ('%s: Rows processed: %d' % (tgtFileName, rows))
+    print ('')
+
+print ('Converting RegistryPurpose rows from CSV file: "%s"', prvFileName)
+print ('Please wait...')
+print ('')
 with io.open(tgtFileName, 'w', encoding='windows-1252') as csvTarget:
     fieldnamesSource = ['pKey','pKeyRegistryUpload','TradeName','Supplier','Purpose']
     fieldnamesTarget = ['pKey','pKeyUpload','TradeName','Supplier','Purpose']
@@ -64,19 +125,20 @@ with io.open(tgtFileName, 'w', encoding='windows-1252') as csvTarget:
     with io.open(ffrCsvPurposeFilePath, 'r', encoding='windows-1252', newline='') as csvSource:
         dictReader = csv.DictReader(csvSource, fieldnames=fieldnamesSource, delimiter='\t', quoting=csv.QUOTE_MINIMAL)
         rows = 0
-        maxRows = 0
-        flushRows = 10000
         thisKey = 0
         for row in dictReader:
             if rows > 0:
                 pKey = row['pKey'].strip()
                 pKeyUpload = row['pKeyRegistryUpload'].strip()
                 try:
+                    ffrPurposeUploadDict[pKey] = ffrUploadDict[pKeyUpload]
+                except KeyError:
+                    ffrPurposeUploadDict[pKey] = None
+                try:
                     pKey = ffrPurposeDict[pKey]
                 except KeyError:
                     thisKey += 1
                     ffrPurposeDict[pKey] = thisKey
-                    ffrPurposeUploadDict[pKey] = ffrUploadDict[pKeyUpload]
                     pKey = thisKey
                 row['pKey'] = pKey
                 row['pKeyUpload'] = ffrUploadDict[pKeyUpload]
@@ -90,10 +152,46 @@ with io.open(tgtFileName, 'w', encoding='windows-1252') as csvTarget:
     print ('%s: Rows processed: %d' % (tgtFileName, rows))
     print ('')
 
-# process the RegistryUploadIngredients.csv file
+# output dictionary for later use the next time this program is executed
+print ('Outputting previous keys to CSV file: "%s"', prvFileName)
+print ('Please wait...')
+print ('')
+rows = 0
+with io.open(prvFileName, 'w', encoding='windows-1252') as csvTarget:
+    dictWriter = csv.DictWriter(csvTarget, fieldnames=['pKey','pInt'], delimiter='\t', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+    dictWriter.writeheader()
+    rows += 1
+    for key, value in ffrPurposeDict.items():
+        rows += 1
+        dictWriter.writerow({'pKey': key, 'pInt': value})
+        if flushRows > 0 and rows % flushRows == 0:
+            print ('Rows processed: %d' % rows)
+    print ('%s: Rows processed: %d' % (prvFileName, rows))
+    print ('')
 
+# process the RegistryUploadIngredients.csv file
+prvFileName = os.path.join(ffrCsvPathName, '%s_prvKeyed.csv' % os.path.splitext(os.path.basename(ffrCsvIngredientsFilePath))[0])
 tgtFileName = os.path.join(ffrCsvPathName, '%s_intKeyed.csv' % os.path.splitext(os.path.basename(ffrCsvIngredientsFilePath))[0])
 
+if os.path.exists(prvFileName):
+    print ('Importing previous keys from RegistryIngredient CSV file: "%s"', prvFileName)
+    print ('Please wait...')
+    print ('')
+    rows = 0
+    with io.open(prvFileName, 'r', encoding='windows-1252', newline='') as csvSource:
+        dictReader = csv.DictReader(csvSource, fieldnames=['pKey','pInt'], delimiter='\t', quoting=csv.QUOTE_MINIMAL)
+        for row in dictReader:
+            rows += 1
+            if rows > 1:
+                ffrIngredientsDict[row['pKey']] = row['pInt']
+            if flushRows > 0 and rows % flushRows == 0:
+                print ('Rows processed: %d' % rows)
+    print ('%s: Rows processed: %d' % (tgtFileName, rows))
+    print ('')
+
+print ('Converting RegistryIngredient rows from CSV file: "%s"', prvFileName)
+print ('Please wait...')
+print ('')
 with io.open(tgtFileName, 'w', encoding='windows-1252') as csvTarget:
     fieldnamesSource = ['pKey','pKeyPurpose','IngredientName','CASNumber','PercentHighAdditive','PercentHFJob','IngredientComment','IngredientMSDS','MassIngredient']
     fieldnamesTarget = ['pKey','pKeyPurpose','pKeyUpload','IngredientName','CASNumber','PercentHighAdditive','PercentHFJob','IngredientComment','IngredientMSDS','MassIngredient']
@@ -102,8 +200,6 @@ with io.open(tgtFileName, 'w', encoding='windows-1252') as csvTarget:
     with io.open(ffrCsvIngredientsFilePath, 'r', encoding='windows-1252', newline='') as csvSource:
         dictReader = csv.DictReader(csvSource, fieldnames=fieldnamesSource, delimiter='\t', quoting=csv.QUOTE_MINIMAL)
         rows = 0
-        maxRows = 0
-        flushRows = 10000
         thisKey = 0
         for row in dictReader:
             if rows > 0:
@@ -126,3 +222,23 @@ with io.open(tgtFileName, 'w', encoding='windows-1252') as csvTarget:
                 break
     print ('%s: Rows processed: %d' % (tgtFileName, rows))
     print ('')
+
+# output dictionary for later use the next time this program is executed
+print ('Outputting previous keys to CSV file: "%s"', prvFileName)
+print ('Please wait...')
+print ('')
+rows = 0
+with io.open(prvFileName, 'w', encoding='windows-1252') as csvTarget:
+    dictWriter = csv.DictWriter(csvTarget, fieldnames=['pKey','pInt'], delimiter='\t', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+    dictWriter.writeheader()
+    rows += 1
+    for key, value in ffrIngredientsDict.items():
+        rows += 1
+        dictWriter.writerow({'pKey': key, 'pInt': value})
+        if flushRows > 0 and rows % flushRows == 0:
+            print ('Rows processed: %d' % rows)
+    print ('%s: Rows processed: %d' % (prvFileName, rows))
+    print ('')
+
+print ('====================')
+print ('Processing finished!')
